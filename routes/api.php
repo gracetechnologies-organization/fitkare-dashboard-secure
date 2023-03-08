a4c0ca3ce2992ff5d761e30fd4801267a7b36ce7
gracetechnologies <appfitkare@gmail.com>
2023-03-08 17:58:45 +0500

    First API Created With All Robust Features

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Category;
+use App\Http\Controllers\ExercisesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('exercises')->group(function(){
+     /** Butt Reduce Routes **/
+     Route::get('/butt_reduce/{cat_id}', [ExercisesController::class, 'listAllDataButtReduce']);
 });
